e28f7745810dcd908870444c8b68bda7b810883a
yyounghoon <yhyoon@huray.net>
2023-07-05 15:05:07 +0900

    [수정] 라우트별 접근 제어 설정

--- src/@shared/components/Root.tsx
+++ src/@shared/components/Root.tsx
@@ -1,7 +1,13 @@
+import { Navigate } from 'react-router-dom';
+import { getLocalStorage } from '../utils/storage';
 import DefaultLayout from './layouts/DefaultLayout';
 import ModalManager from './ModalManager';
 
 function Root() {
+  const isLogin = getLocalStorage('accessToken');
+
+  if (!isLogin) return <Navigate to={'/login'} />;
+
   return (
     <>
       <DefaultLayout />
